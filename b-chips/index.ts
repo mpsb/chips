00b31cab5239875c75dfc29de72f6abfbe916098
mpsb <mps.bilo@gmail.com>
2022-06-15 22:07:05 +0800

    nexus setup

--- b-chips/index.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloServer } from 'apollo-server';
-
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({ typeDefs, resolvers, csrfPrevention: true });
-  const { url } = await server.listen();
-  console.log(`🚀 Server ready at ${url}`);
-}
